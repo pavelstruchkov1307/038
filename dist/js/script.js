1a396678fb6bfc838dbc722eb4daf7cc76ac9973
Pavel <pavel.struchkov1999@gmail.com>
2023-12-07 19:50:25 +0300

    timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+
+//tabs
 const tabs = document.querySelectorAll(".tabheader__item"),
       tabsParent = document.querySelector(".tabheader__items"),
       tabContent = document.querySelectorAll(".tabcontent");
@@ -35,3 +37,49 @@ tabsParent.addEventListener("click", (e) => {
 
 hideTabContext();
 showTabContent();
+
+//timer
+
+const endTime = "2023-12-31";
+
+function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector("#days"),
+          hours = timer.querySelector("#hours"),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector("#seconds");
+
+    const timerId = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+
+        const t = getDateDifference(endTime);
+
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+
+    }
+}
+
+function getDateDifference(endTime) {
+    const diff = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(diff / (1000 * 60 * 60 * 24) % 30),
+          hours = Math.floor((diff / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor((diff / (1000 * 60)) % 60),
+          seconds = Math.floor((diff / 1000) % 60);
+
+    return {
+        total: diff,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+   
+}
+
+setClock(".timer",endTime);
