c8af86970fba5a3a55ce7f88189e2c54f9a54c3f
Pavel <pavel.struchkov1999@gmail.com>
2025-02-04 18:52:58 +0300

    class

--- dist/js/script.js
+++ dist/js/script.js
@@ -152,3 +152,36 @@ window.addEventListener("DOMContentLoaded", () =>   {
 
 });
 
+// class
+
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+    }
+
+    render() {
+        const card = document.createElement("div");
+
+        card.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> рублей</div>
+                </div>
+            </div>
+        `;
+
+        this.parent.append(card);
+    }
+};
+
+const vegy = new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, ".menu .container").render();
